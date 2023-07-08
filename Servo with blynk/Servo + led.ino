b377a6f680e215a23aa28972dd638ba5cbe9a4aa
budilutfii <85115264+budilutfii@users.noreply.github.com>
2023-07-08 15:20:35 +0700

    Update Servo + led.ino

--- Servo with blynk/Servo + led.ino
+++ Servo with blynk/Servo + led.ino
@@ -12,8 +12,8 @@
 Servo servo5;
 
 char auth[] = BLYNK_AUTH_TOKEN;
-char ssid[] = "tselhome-906B"; // Change your Wifi/ Hotspot Name
-char pass[] = "Ayambakar12345"; // Change your Wifi/ Hotspot Password
+char ssid[] = ""; // Change your Wifi/ Hotspot Name
+char pass[] = ""; // Change your Wifi/ Hotspot Password
 
 BLYNK_WRITE(V4)
 {
